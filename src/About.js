f7d19a52098c8ca8d74bc5ed90df091dbdb39cd3
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-02-23 16:47:20 -0800

    Completed Skills section of portfolio

--- src/About.js
+++ src/About.js
@@ -10,9 +10,9 @@ export default function About () {
           <img src="https://media-exp1.licdn.com/dms/image/C5603AQHGcsjvm92RiA/profile-displayphoto-shrink_200_200/0/1609202193871?e=1619049600&v=beta&t=4-EM0czab9XYWY5Rx_Kh40zMcCN5AHnRwsZD0bsikEw"/>
         </div>
         <div className="two-col">
-          <p>Hello there! I'm Samantha.</p>
-          <p>I am a full-stack web developer. </p>
-          <p>My side projects include learning new languages, reading, playing video games, and drawing. I can speak English, French, and Cantonese and I am currently learning Portuguese and American Sign Language. My favourite book is J.R.R. Tolkein's "The Hobbit". I love logic puzzles and I am very good at Minesweeper. I enjoy creating digital art, but always prefer good ol' fashion pen and paper. Check out my <a id="inline" href="https://www.instagram.com/samfokdraws/">Instagram</a> to see some of my work!</p>
+          <p>Hello there, hi there, ho there! <br/>I'm Samantha and I am a full-stack web developer based in Vancouver, CA.</p>
+          <p>I grew up with a passion for art and creativity. I was the kid that would draw on their napkin at the dinner table and make handmade birthday cards. This passion lead me to pursue a career in web development. I love building and designing things for the web and working on a platform that is constantly growing.</p>
+          <p>My side projects include learning new languages, reading, playing video games, and drawing. Check out my <a id="inline" href="https://www.instagram.com/samfokdraws/">Instagram</a> to see some of my art!</p>
         </div>
       </section>
     </main>
--- src/Skills.js
+++ src/Skills.js
@@ -1,11 +1,99 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3Alt, faJsSquare, faNodeJs, faReact, faBootstrap, faSass } from '@fortawesome/free-brands-svg-icons';
+import { faGem, faDatabase } from '@fortawesome/free-solid-svg-icons';
 import "./Skills.css";
 
 export default function Skills () {
   return (
     <main id="skills" className="skillspage">
       <section className="skills-container">
-        <h1>Skills Page</h1>
+        <h1>Skills</h1>
+
+        <div className="col-container">
+          <div className="three-col">
+            <h4>Languages</h4>
+            <ul>
+              <li className="contact-item">
+                <FontAwesomeIcon className="contact-icon" icon={faHtml5}/>
+                HTML
+              </li>
+              <li className="contact-item">
+                <FontAwesomeIcon className="contact-icon" icon={faCss3Alt}/>
+                CSS
+              </li>
+              <li className="contact-item">
+                <FontAwesomeIcon className="contact-icon" icon={faJsSquare}/>
+                Javascript
+              </li>
+              <li className="contact-item">
+                <FontAwesomeIcon className="contact-icon" icon={faGem}/>
+                Ruby
+              </li>
+              <li className="contact-item">
+                <FontAwesomeIcon className="contact-icon" icon={faDatabase}/>
+                SQL
+              </li>
+            </ul>
+          </div>
+
+          <div className="three-col">
+            <h4>Frameworks/Libraries</h4>
+            <ul>
+              <li className="contact-item">
+                  <FontAwesomeIcon className="contact-icon" icon={faNodeJs}/>
+                  NodeJS
+              </li>
+              <li className="contact-item">
+                  <FontAwesomeIcon className="contact-icon" icon={faReact}/>
+                  ReactJS
+              </li>
+              <li className="contact-item">
+                  <FontAwesomeIcon className="contact-icon" icon={faBootstrap}/>
+                  Bootstrap
+              </li>
+              <li className="contact-item">
+                  <FontAwesomeIcon className="contact-icon" icon={faSass}/>
+                  SCSS
+              </li>
+              <li className="contact-item">
+                Material-UI
+              </li>
+              <li className="contact-item">
+                ExpressJS
+              </li>
+              <li className="contact-item">
+                JQuery
+              </li>
+              <li className="contact-item">
+                Axios
+              </li>
+              <li className="contact-item">
+                Ajax
+              </li>
+            </ul>
+          </div>
+          <div className="three-col">
+            <h4>Databases</h4>
+            <ul>
+              <li className="contact-item">
+                PostgreSQL
+              </li>
+            </ul>
+          </div>
+          <div className="three-col">
+            <h4>Other</h4>
+            <ul>
+              <li className="contact-item">
+                GSAP
+              </li>
+              <li className="contact-item">
+                Adobe Illustrator
+              </li>
+            </ul>
+          </div>
+        </div>
+
       </section>
     </main>
   )
