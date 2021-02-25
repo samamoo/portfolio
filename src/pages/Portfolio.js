9ab76fd1155e76bbd9e1ac48ac3201ad2b67dae9
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-02-24 23:24:42 -0800

    Moved portfolio components to pages folder

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Portfolio.css";
+import PortfolioItem from "./PortfolioItem";
+import data from "../projectsDb.json";
+
+export default function Portfolio () {
+  return (
+    <main id="portfolio" className="portfoliopage">
+      <section className="portfolio-container">
+        <h1>Portfolio</h1>
+        <div className="item-container">
+          <ul className="portfolio-list">
+            {data.map((val) => {
+              return (
+                <PortfolioItem
+                title={val.title}
+                description={val.description}
+                stack={val.stack}
+                image={val.image}
+                github={val.github} />
+              )
+            })}
+          </ul>
+        </div>
+      </section>
+    </main>
+  )
+}
